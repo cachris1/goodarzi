dcb2246eeb94e1a5ebf14d02982ad92268b0fea8
cachris1 <60014142+cachris1@users.noreply.github.com>
2023-04-03 11:15:58 -0700

    Delete lrpath.R

--- lrpath.R
+++
@@ -1,7 +0,0 @@
-gos <- read.delim("./file573847206.txt")
-bp <- gos[gos["ConceptType"] == "GOBP",]
-bp_sig <- bp[bp["FDR"] < 0.05,]
-sperms <- filter(gos, grepl("sperm", Name))
-immune <- filter(gos, grepl("immune", Name))
-virus <- filter(gos, grepl("vir", Name))
-print(nrow(bp_sig))
