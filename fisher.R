c4ce2d9952c82cf78ba01553c81104c3920fb715
cachris1 <60014142+cachris1@users.noreply.github.com>
2023-04-03 11:13:51 -0700

    Delete fisher.R

--- fisher.R
+++
@@ -1,5 +0,0 @@
-timer2 <- read.csv("../timer2.csv", row.names = 1)
-#timer2 <- timer2[timer2$HPVint == "pos",]
-fish <- data.frame(table(timer2[timer2$e.bin == 1,]$Cohort))
-fish[,3] <- (table(timer2[timer2$e.bin == 0,]$Cohort))
-fish <- fish[-1]
